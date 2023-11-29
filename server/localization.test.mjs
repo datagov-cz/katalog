d4862c821fd05c3be77ea881302b9efe3a69c0e5
Petr Škoda <skodapetr@gmail.com>
2023-11-29 12:32:24 +0100

    Add empty test

---
+++ server/localization.test.mjs
@@ -0,0 +1,10 @@
+import assert from "node:assert";
+import { describe, it } from "node:test"
+
+describe("Localization", () => {
+
+  it("Be true", () => {
+    assert.strictEqual(2, 1+1);
+  });
+
+});
