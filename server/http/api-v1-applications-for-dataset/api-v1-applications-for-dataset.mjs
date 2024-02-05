cf5ba684df2bb37eb04e8ad62e538eebcd5e1263
Petr Škoda <skodapetr@gmail.com>
2024-02-05 13:17:39 +0100

    Fix applicattions for dataset endpoint

--- server/http/api-v1-applications-for-dataset/api-v1-applications-for-dataset.mjs
+++ server/http/api-v1-applications-for-dataset/api-v1-applications-for-dataset.mjs
@@ -1,10 +1,13 @@
 export default async function handleRequest(services, request, reply) {
   const query = parseQuery(request);
-  let payload = [];
+  let applications = [];
   if (isQueryValid(query)) {
-    payload = await services.solrApplication.fetchApplicationsWithDatasets(
+    applications = await services.solrApplication.fetchApplicationsWithDatasets(
       query["languages"], query["datasets"]);
   }
+  const payload = {
+    "applications": applications,
+  };
   reply
     .code(200)
     .header("Content-Type", "text/json; charset=utf-8").send(payload);
